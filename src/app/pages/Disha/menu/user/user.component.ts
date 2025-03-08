d65ff580cf2ae1d279d3a2eab2c820fc51b86327
Amit <amitmaity2024@gmail.com>
2025-03-08 20:12:27 +0530

    user complite

--- src/app/pages/Disha/menu/user/user.component.ts
+++ src/app/pages/Disha/menu/user/user.component.ts
@@ -1,22 +1,103 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { DatePickerModule } from 'primeng/datepicker';
+import { firstValueFrom, tap } from 'rxjs';
+import { AlertService } from '../../../../../services/alert.service';
+import { UserService } from '../../../../../services/user.service';
 
 @Component({
   selector: 'app-user',
-  imports: [DialogModule, ButtonModule, FormsModule,CommonModule,InputTextModule,
-    DatePickerModule,DropdownModule],
+  imports: [DialogModule, ButtonModule, FormsModule, CommonModule, InputTextModule,
+    DatePickerModule, DropdownModule, ReactiveFormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
   showAddState: boolean = false;
+  userList?: any[];
+  addUserForm: FormGroup;
+  private touchStartY: number = 0;
+  dropdownOptions = [{ label: 'Male', value: 'M' }, { label: 'Female', value: 'F' }];
+  selectedValue: any;
+
+
+
+  constructor(private alertService: AlertService, private userService: UserService, private fb: FormBuilder) {
+    this.addUserForm = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      address: ['', Validators.required],
+      mobile: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      birth_date: [null, Validators.required],
+      gender: [null, Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.gateAllUser();
+  }
+
+  
+  // gate all user 
+  async gateAllUser() {
+    try {
+      await firstValueFrom(this.userService.getAllUsers(0).pipe(
+        tap(
+          (res) => {
+            if (res.body) {
+              if (res.body) {
+                this.userList = res.body;
+              }
+            }
+          },
+          (error) => {
+            this.alertService.error(error.error.message);
+          }
+        )
+      ))
+    }
+    catch (error) {
+      this.alertService.error("Server connection failure please try agen !")
+    }
+  }
+
+
+   // create a new user
+  async onSave() {
+    if (this.addUserForm.valid) {
+      try {
+        await firstValueFrom(this.userService.addNewUser(this.addUserForm.value).pipe(
+          tap(
+            (res) => {
+              if (res.body) {
+                this.alertService.success('User created successfully');
+                this.showAddState = false;
+                this.gateAllUser();
+              }
+            },
+            (error) => {
+              this.alertService.error(error.error.message);
+            }
+          )
+        ))
+      } catch {
+        this.alertService.error("Server connection failure please try agen !")
+      }
+    } else {
+      this.addUserForm.markAllAsTouched(); // Show all validation errors
+    }
+  }
+
+
+
+
 
   toggleAddState() {
     this.showAddState = !this.showAddState;
@@ -37,13 +118,7 @@ export class UserComponent {
     }
   }
 
-  private touchStartY: number = 0;
+  
 
 
-
-  dropdownOptions = [
-    { label: 'Male', value: 'M' },
-    { label: 'Female', value: 'F' }
-  ];
-  selectedValue: any;
 }
