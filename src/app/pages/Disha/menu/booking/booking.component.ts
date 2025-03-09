4c372c206c2eb37d2499635455c905b4190dab12
Amit <amitmaity2024@gmail.com>
2025-03-09 16:42:49 +0530

    booking
    UI and dropdown Update

--- src/app/pages/Disha/menu/booking/booking.component.ts
+++ src/app/pages/Disha/menu/booking/booking.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CityService } from '../../../../../services/city.service';
 import { StateService } from '../../../../../services/state.service';
 import { BranchService } from '../../../../../services/branch.service';
@@ -9,12 +9,14 @@ import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { MessageService } from 'primeng/api';
 import { firstValueFrom, tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { AlertService } from '../../../../../services/alert.service';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss'],
-  imports: [DropdownModule, ButtonModule, FormsModule, InputTextModule],
+  imports: [DropdownModule, ButtonModule, FormsModule, InputTextModule, ReactiveFormsModule, CommonModule],
   providers: [MessageService]
 })
 export class BookingComponent implements OnInit {
@@ -28,14 +30,39 @@ export class BookingComponent implements OnInit {
   selectedCity: any;
   selectedBranch: any;
   selectedTransportMode: any;
-
+  bookingForm: FormGroup;
   constructor(
     private cityService: CityService,
     private stateService: StateService,
     private branchService: BranchService,
     private bookingService: BookingService,
-    private messageService: MessageService
-  ) {}
+    private messageService: MessageService,
+    private fb: FormBuilder,
+    private alertService: AlertService
+  ) {
+    this.bookingForm = this.fb.group({
+      slip_no: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      consignor_name: ['', [Validators.required, Validators.pattern("^[a-zA-Z ]*$")]],
+      consignor_mobile: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      consignee_name: ['', Validators.required, Validators.pattern("^[a-zA-Z ]*$")],
+      consignee_mobile: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      destination_city_id: [null, Validators.required],
+      destination_branch_id: [null, Validators.required],
+      transport_mode: [null, Validators.required],
+      count: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      weight: ['', [Validators.required, Validators.pattern('^[0-9]+(.[0-9]+)?$')]],
+      value: ['', Validators.pattern('^[0-9]+$')],
+      contents: [''],
+      charges: ['', [Validators.required, Validators.pattern('^[0-9]+(.[0-9]+)?$')]],
+      shipper: ['', Validators.pattern('^[0-9]+(.[0-9]+)?$')],
+      other: ['', Validators.pattern('^[0-9]+(.[0-9]+)?$')],
+      cgst: ['', [Validators.required, Validators.pattern('^[0-9]+(.[0-9]+)?$')]],
+      sgst: ['', [Validators.required, Validators.pattern('^[0-9]+(.[0-9]+)?$')]],
+      igst: ['', [Validators.required, Validators.pattern('^[0-9]+(.[0-9]+)?$')]],
+      total: ['', [Validators.required, Validators.pattern('^[0-9]+(.[0-9]+)?$')]],
+    });
+
+  }
 
   ngOnInit(): void {
     this.loadStates();
@@ -46,22 +73,21 @@ export class BookingComponent implements OnInit {
   async loadStates() {
     await firstValueFrom(this.stateService.getAllStates(0).pipe(
       tap(
-        (res)=>{
-          if(res.body){
+        (res) => {
+          if (res.body) {
             this.states = res.body;
           }
         }
       )
     ));
-
   }
 
-  async onStateChange() {
-    if (this.selectedState) {
-      await firstValueFrom(this.cityService.getCitiesByStateId(this.selectedState).pipe(
+  async onStateChange($event: any) {
+    if ($event) {
+      await firstValueFrom(this.cityService.getCitiesByStateId($event).pipe(
         tap(
-          (res)=>{
-            if(res.body){
+          (res) => {
+            if (res.body) {
               this.cities = res.body;
             }
           }
@@ -73,12 +99,12 @@ export class BookingComponent implements OnInit {
     }
   }
 
-  async onCityChange() {
-    if (this.selectedCity) {
-      await firstValueFrom(this.branchService.getBranchesByCityId(this.selectedCity).pipe(
+  async onCityChange($event: any) {
+    if ($event) {
+      await firstValueFrom(this.branchService.getBranchesByCityId($event, 0).pipe(
         tap(
-          (res)=>{
-            if(res.body){
+          (res) => {
+            if (res.body) {
               this.branches = res.body;
             }
           }
@@ -118,4 +144,28 @@ export class BookingComponent implements OnInit {
       }
     });
   }
+
+
+  async saveBooking() {
+    try {
+      if (this.bookingForm.valid) {
+        const booking = this.bookingForm.value;
+        const response = await this.bookingService.addNewBooking(booking).toPromise();
+
+        if (response.success) {
+          this.alertService.success(response.message);
+          this.bookingForm.reset();
+          this.bookingForm.markAsUntouched();
+        } else {
+          this.alertService.error(response.message);
+        }
+      } else {
+        this.alertService.error('Please fill out all required fields correctly.');
+      }
+    } catch (error) {
+      console.error('Error saving booking:', error);
+      this.alertService.error('Something went wrong. Please try again later.');
+    }
+  }
+
 }
