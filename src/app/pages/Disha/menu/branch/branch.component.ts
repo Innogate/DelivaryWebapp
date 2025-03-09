00b325a6363b90ae548082e01de87a50806afff0
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-03-09 10:21:46 +0530

    Brach added

--- src/app/pages/Disha/menu/branch/branch.component.ts
+++ src/app/pages/Disha/menu/branch/branch.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
-
+import { BranchService } from '../../../../../services/branch.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-branch',
-  imports: [],
   templateUrl: './branch.component.html',
-  styleUrl: './branch.component.scss'
+  styleUrls: ['./branch.component.scss'],
+  imports: [CommonModule]
 })
 export class BranchComponent {
+  branchList: any[] = [];
 
+  constructor(private branchService: BranchService) {
+    this.fetchBranches();
+  }
+
+  fetchBranches() {
+    this.branchService.getAllBranches().subscribe(response => {
+      if (response.status === 200 && response.body) {
+        this.branchList = response.body;
+      }
+    });
+  }
+
+  deleteBranch(branch: any) {
+    this.branchService.deleteBranch(branch.id).subscribe(() => {
+      this.branchList = this.branchList.filter(b => b.id !== branch.id);
+    });
+  }
+
+  updateBranch(branch: any) {
+    console.log('Update branch', branch);
+  }
+
+  addBranch() {
+    console.log('Add branch');
+  }
 }
