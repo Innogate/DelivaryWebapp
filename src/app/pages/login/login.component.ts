b8f53515b1b22466d554dbf754d3e5d4c6b9a0dd
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-03-17 14:47:04 +0000

    Fixed booking status

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,56 +1,64 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormsModule } from '@angular/forms';
+import { firstValueFrom, tap } from 'rxjs';
+import { Router } from '@angular/router';
 import { GlobalStorageService } from '../../../services/global-storage.service';
 import { LoginService } from '../../../services/login.service';
 import { AlertService } from '../../../services/alert.service';
-import { FormGroup } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { firstValueFrom, tap } from 'rxjs';
-import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  standalone: true
 })
 export class LoginComponent implements OnInit {
   userFrom?: FormGroup;
+  passwordVisible = false; // Variable to control password visibility
+
   constructor(
     private loginService: LoginService,
     private globalStorageService: GlobalStorageService,
     private alertService: AlertService,
     private fb: FormBuilder,
     private router: Router
-  ){}
+  ) {}
 
   ngOnInit(): void {
     this.userFrom = this.fb.group({
       id: ['', [Validators.required, Validators.min(10)]],
       passwd: ['', [Validators.required, Validators.minLength(4)]],
-    })
+    });
+  }
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible; // Toggle password visibility
   }
 
   async onSubmit() {
     if (this.userFrom?.valid) {
-      await firstValueFrom(this.loginService.login(this.userFrom.value).pipe(
-        tap(
-          (res) => {
-            if (res.body) {
-              this.globalStorageService.set('token', res.body.token, true);
-              this.router.navigate(['/']);
+      await firstValueFrom(
+        this.loginService.login(this.userFrom.value).pipe(
+          tap(
+            (res) => {
+              if (res.body) {
+                this.globalStorageService.set('token', res.body.token, true);
+                this.router.navigate(['/']);
+              }
+            },
+            (error) => {
+              this.alertService.error(error.error.message);
             }
-          },
-          (error) => {
-            this.alertService.error(error.error.message);
-          }
+          )
         )
-      ))
-    }else{
+      );
+    } else {
       this.userFrom?.markAllAsTouched();
     }
   }
