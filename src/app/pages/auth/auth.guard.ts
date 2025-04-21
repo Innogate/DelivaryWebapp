45df6357585646ca7b9ee1c8083b2af8a4c31cfc
Amit <amitmaity2024@gmail.com>
2025-04-21 20:35:35 +0530

    fix sume issue

--- src/app/pages/auth/auth.guard.ts
+++ src/app/pages/auth/auth.guard.ts
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate {
       return true;
     } else {
         // return true
-      this.router.navigate(['/pages/login']);
+      this.router.navigate(['/pages/home']);
       return false;
     
     }
